aba13d9c336292f5bb3ba6738f1dce4a71d41e47
Jerlin Joseph <jerlin.joseph@protonmail.com>
2018-11-24 05:22:42 +0000

    Initial checkin

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe PagesController, type: :controller do
+
+  describe "GET #home" do
+    it "returns http success" do
+      get :home
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #faq" do
+    it "returns http success" do
+      get :faq
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #science" do
+    it "returns http success" do
+      get :science
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
